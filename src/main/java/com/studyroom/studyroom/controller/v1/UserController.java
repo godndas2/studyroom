b5029cd58bdf279250dd3c40aede79bf0d7ca2ff
halfdev <gusgodnd@naver.com>
2019-09-18 00:29:52 +0900

    google oauth2.0 ...

--- src/main/java/com/studyroom/studyroom/controller/v1/UserController.java
+++ src/main/java/com/studyroom/studyroom/controller/v1/UserController.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 /*
 * 단일 처리 : getBasicResult()
