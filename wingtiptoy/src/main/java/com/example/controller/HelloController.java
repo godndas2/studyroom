57cfa43aff48ba7db36399a3f50597e39d5415da
halfdev <gusgodnd@naver.com>
2019-09-30 23:34:49 +0900

    #1 aad api test...

--- wingtiptoy/src/main/java/com/example/controller/HelloController.java
+++ wingtiptoy/src/main/java/com/example/controller/HelloController.java
@@ -1,36 +1,36 @@
-package com.example.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-@RequiredArgsConstructor
-public class HelloController {
-
-    private final OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
-
-    @PreAuthorize("hasRole('Users')")
-    @GetMapping("user")
-    @ResponseBody
-    public String helloWorld() {
-        return "It's User-s!";
-    }
-
-    @GetMapping("/")
-    public String index(Model model, OAuth2AuthenticationToken authentication) {
-        final OAuth2AuthorizedClient authorizedClient =
-                this.oAuth2AuthorizedClientService.loadAuthorizedClient(
-                        authentication.getAuthorizedClientRegistrationId(),
-                        authentication.getName());
-        model.addAttribute("userName", authentication.getName());
-        model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
-        return "index";
-    }
-}
+//package com.example.controller;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+//import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+//import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//@RequiredArgsConstructor
+//public class HelloController {
+//
+//    private final OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+//
+//    @PreAuthorize("hasRole('Users')")
+//    @GetMapping("user")
+//    @ResponseBody
+//    public String helloWorld() {
+//        return "It's User-s!";
+//    }
+//
+//    @GetMapping("/")
+//    public String index(Model model, OAuth2AuthenticationToken authentication) {
+//        final OAuth2AuthorizedClient authorizedClient =
+//                this.oAuth2AuthorizedClientService.loadAuthorizedClient(
+//                        authentication.getAuthorizedClientRegistrationId(),
+//                        authentication.getName());
+//        model.addAttribute("userName", authentication.getName());
+//        model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
+//        return "index";
+//    }
+//}
