36b562b4e02b833c8c6b499cc50e437490468614
halfdev <gusgodnd@naver.com>
2019-11-24 00:36:42 +0900

    ref #5 how to sign in (import.sql, security modified)

--- cafe/src/main/java/org/halfdev/security/SecurityConfig.java
+++ cafe/src/main/java/org/halfdev/security/SecurityConfig.java
@@ -1,12 +1,28 @@
 package org.halfdev.security;
 
+import lombok.RequiredArgsConstructor;
+import org.halfdev.security.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+    private final CorsFilter corsFilter;
+    // TODO Handler.class
 
     @Override
     public void configure(WebSecurity web) {
@@ -24,5 +40,49 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/h2-console/**"
                 );
     }
+
+    // Configure BCrypt password encoder =====================================================================
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    // Configure security settings ===========================================================================
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // we don't need CSRF because our token is invulnerable
+                .csrf().disable()
+
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+
+                // enable h2-console
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // create no session
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/authenticate").permitAll()
+                // .antMatchers("/api/register").permitAll()
+                // .antMatchers("/api/activate").permitAll()
+                // .antMatchers("/api/account/reset-password/init").permitAll()
+                // .antMatchers("/api/account/reset-password/finish").permitAll()
+
+
+                // Check, client.js
+                .antMatchers("/api/person").hasAuthority("ROLE_USER")
+                .antMatchers("/api/hiddenmessage").hasAuthority("ROLE_ADMIN")
+
+                .anyRequest().authenticated();
+
+    }
+
 
 }
