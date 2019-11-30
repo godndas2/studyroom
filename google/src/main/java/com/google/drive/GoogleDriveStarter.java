eeeaf4daf90cde274b63584ac9fda0148d05e705
halfdev <gusgodnd@naver.com>
2019-12-01 00:30:29 +0900

    hotfix - delete GoogleDriveStarter.java

--- google/src/main/java/com/google/drive/GoogleDriveStarter.java
+++ google/src/main/java/com/google/drive/GoogleDriveStarter.java
@@ -1,89 +1,89 @@
-package com.google.drive;
-
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.DriveScopes;
-import com.google.api.services.drive.model.File;
-import com.google.api.services.drive.model.FileList;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.security.GeneralSecurityException;
-import java.util.Collections;
-import java.util.List;
-
-public class GoogleDriveStarter {
-
-    private static final String APPLICATION_NAME = "GOOGLE DRIVE API STARTER";
-    private static final JacksonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
-
-    /**
-    * @author halfdev
-    * @since 2019-11-26
-    * Global instance of the scopes required by this APP
-    * If modifying these scopes, delete your previously saved tokens/ folder.
-    */
-    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_METADATA_READONLY);
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
-
-    /**
-    * @author halfdev
-    * @since 2019-11-26
-    * Creates an authorized Credential object.
-    */
-    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
-
-        // Load Client secrets
-        InputStream in = GoogleDriveStarter.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-
-        if (in == null) {
-            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-        }
-
-        GoogleClientSecrets googleClientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-        GoogleAuthorizationCodeFlow codeFlow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, googleClientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
-                .build();
-
-        LocalServerReceiver localServerReceiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return  new AuthorizationCodeInstalledApp(codeFlow, localServerReceiver).authorize("user");
-    }
-
-    public static void main(String... args) throws IOException, GeneralSecurityException {
-        // Build a new Auth API Client service
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Drive drive = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-
-        // print the names and ID for up to 10 files
-        FileList fileList = drive.files().list()
-                .setPageSize(10)
-                .setFields("nextPage Token, files(id, name)")
-                .execute();
-
-        List<File> files = fileList.getFiles();
-        if (files == null || files.isEmpty()) {
-            System.out.println("NO FILES FOUND");
-        } else {
-            System.out.println("FILES : ");
-            for (File file : files) {
-                System.out.printf("%s (%s)\n", file.getName(), file.getId());
-            }
-        }
-    }
-}
+//package com.google.drive;
+//
+//import com.google.api.client.auth.oauth2.Credential;
+//import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+//import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+//import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+//import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+//import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+//import com.google.api.client.http.javanet.NetHttpTransport;
+//import com.google.api.client.json.jackson2.JacksonFactory;
+//import com.google.api.client.util.store.FileDataStoreFactory;
+//import com.google.api.services.drive.Drive;
+//import com.google.api.services.drive.DriveScopes;
+//import com.google.api.services.drive.model.File;
+//import com.google.api.services.drive.model.FileList;
+//
+//import java.io.FileNotFoundException;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.security.GeneralSecurityException;
+//import java.util.Collections;
+//import java.util.List;
+//
+//public class GoogleDriveStarter {
+//
+//    private static final String APPLICATION_NAME = "GOOGLE DRIVE API STARTER";
+//    private static final JacksonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+//    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+//
+//    /**
+//    * @author halfdev
+//    * @since 2019-11-26
+//    * Global instance of the scopes required by this APP
+//    * If modifying these scopes, delete your previously saved tokens/ folder.
+//    */
+//    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_METADATA_READONLY);
+//    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+//
+//    /**
+//    * @author halfdev
+//    * @since 2019-11-26
+//    * Creates an authorized Credential object.
+//    */
+//    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+//
+//        // Load Client secrets
+//        InputStream in = GoogleDriveStarter.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+//
+//        if (in == null) {
+//            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+//        }
+//
+//        GoogleClientSecrets googleClientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+//
+//        GoogleAuthorizationCodeFlow codeFlow = new GoogleAuthorizationCodeFlow.Builder(
+//                HTTP_TRANSPORT, JSON_FACTORY, googleClientSecrets, SCOPES)
+//                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+//                .setAccessType("offline")
+//                .build();
+//
+//        LocalServerReceiver localServerReceiver = new LocalServerReceiver.Builder().setPort(8888).build();
+//        return  new AuthorizationCodeInstalledApp(codeFlow, localServerReceiver).authorize("user");
+//    }
+//
+//    public static void main(String... args) throws IOException, GeneralSecurityException {
+//        // Build a new Auth API Client service
+//        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+//        Drive drive = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+//                .setApplicationName(APPLICATION_NAME)
+//                .build();
+//
+//        // print the names and ID for up to 10 files
+//        FileList fileList = drive.files().list()
+//                .setPageSize(10)
+//                .setFields("nextPage Token, files(id, name)")
+//                .execute();
+//
+//        List<File> files = fileList.getFiles();
+//        if (files == null || files.isEmpty()) {
+//            System.out.println("NO FILES FOUND");
+//        } else {
+//            System.out.println("FILES : ");
+//            for (File file : files) {
+//                System.out.printf("%s (%s)\n", file.getName(), file.getId());
+//            }
+//        }
+//    }
+//}
